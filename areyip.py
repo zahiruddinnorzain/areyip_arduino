e168e1e7d79fdd78833535fa2cb46a2b9576fca0
zahiruddin <kepaknaga@gmail.com>
2018-11-11 21:56:23 +0800

    add blynk function
    and fix some bug

--- areyip.py
+++ areyip.py
@@ -1,9 +1,12 @@
 
 projectName = "outfile"
+f = open(projectName + ".txt", "w")
 
 def start():
+          
     f = open(projectName + ".txt", "w")
-    f.write("void main(){ \n\n")
+    f.write("void main(){ \nSerial.begin(9600);\n\n")
+
 
         
 def mid():
@@ -19,6 +22,9 @@ def delay(x):
     f = open(projectName + ".txt", "a")
     f.write("delay(" + str(x) + ");\n")
 
+def show(x):
+    f = open(projectName + ".txt", "a")
+    f.write("Serial.println(" + str(x) + ");\n")
 
 class SdeclarePin:
     def __init__(self, pin, kondisi):
@@ -40,14 +46,38 @@ class SdeclarePin:
         if self.kondisi != "OUTPUT":
             print("ERROR: CANNOT READ INPUT. CHANGE INPUT TO OUTPUT AT PIN " + str(self.pin) + "\n")
         
-    #sensor = analogRead(pin)
     def WanalogRead(self, datain):
         self.datain = datain
         
         if self.kondisi == "INPUT":
         
             f = open(projectName + ".txt", "a")
-            f.write(datain + " = analogRead(" + str(self.pin) + ");" )
+            f.write("float " + datain + " = analogRead(" + str(self.pin) + ");\n" )
             
         if self.kondisi != "INPUT":
             print("ERROR: CANNOT READ OUTPUT. CHANGE OUTPUT TO INPUT AT PIN " + str(self.pin) + "\n")
+#=====================
+class Sblynk:
+    def __init__(self, auth, ssid, password):
+        self.auth = auth
+        self.ssid = ssid
+        self.password = password
+
+    def startBlynk(self):
+        
+        f = open(projectName + ".txt", "w")
+        f.write("#include <BlynkSimpleEsp8266.h>\n" )
+                
+        f.write('char auth[] = "' + self.auth + '";\n' )
+        f.write('char ssid[] = "' + self.ssid + '";\n' )
+        f.write('char pass[] = "' + self.password + '";\n\n' )
+        
+        f.write("void main(){ \nSerial.begin(9600);\n\n")
+        f.write("Blynk.begin(auth, ssid, pass);\n\n")
+    
+    
+    
+    
+    
+    
+    
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import areyip
 areyip.projectName = "zero33"
 r1 = areyip.SdeclarePin(13,"OUTPUT")
 r2 = areyip.SdeclarePin(12,"INPUT")
+r3 = areyip.Sblynk("abc7","botak","ppkk")
 
-
-areyip.start()
+r3.startBlynk()
+#areyip.start()
 
 r1.WdeclarePin()
 r2.WdeclarePin()
@@ -18,5 +19,6 @@ areyip.delay(2)
 r1.WdigitalPin("LOW")
 areyip.delay(1)
 r2.WanalogRead("sensor")
+areyip.show("sensor")
 
 areyip.end()
